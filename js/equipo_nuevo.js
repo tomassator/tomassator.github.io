5ba9d871901d8a25c57080c35d695145e3c70a5c
Tomas Sator <69409433+tomassator@users.noreply.github.com>
2023-07-08 19:03:52 -0300

    Update equipo_nuevo.js

--- js/equipo_nuevo.js
+++ js/equipo_nuevo.js
@@ -1,8 +1,8 @@
 function guardar() {
     let n = document.getElementById("nombre").value
     let a = document.getElementById("apellido").value
-    let d = parseFloat(document.getElementById("DNI").value)
-    let t = parseInt(document.getElementById("telefono").value)
+    let d = document.getElementById("DNI").value
+    let t = document.getElementById("telefono").value
     
 
 
